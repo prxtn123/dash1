21a1204fb76f4526cc5b6900931dc8414f4ade31
prxtn123 <tom.noble@praxisgb.com>
2026-02-27 12:29:02 +0000

    amplify: force-add aws-exports.js for Amplify build

---
+++ src/aws-exports.js
@@ -0,0 +1,13 @@
+/**
+ * AWS Amplify Configuration
+ * Cognito User Pool: SJSU_GU_User_Pool (imported)
+ * Region: us-east-1
+ */
+const awsconfig = {
+  aws_project_region: 'us-east-1',
+  aws_cognito_region: 'us-east-1',
+  aws_user_pools_id: 'us-east-1_yZDcfnqL2',
+  aws_user_pools_web_client_id: '6bqg92qj6okmi6jgv8b2an9po9',
+};
+
+export default awsconfig;
